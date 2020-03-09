c1e49b39e76f0e487b938965900eac630d48e137
DDD-Rock <laiyifa@bk.ru>
2020-03-09 18:43:20 +0800

    骨架1

---
+++ nobug-shop-zuul/src/main/java/com/nobug/NobugShopZuulApplication.java
@@ -0,0 +1,13 @@
+package com.nobug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobugShopZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NobugShopZuulApplication.class, args);
+    }
+
+}
