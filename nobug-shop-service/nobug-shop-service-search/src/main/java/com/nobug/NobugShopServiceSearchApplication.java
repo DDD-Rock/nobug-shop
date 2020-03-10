363e8971fcf66a2b9097c9fdc8f71e0b05abe6e3
aixiaodevgghh <405956256@qq.com>
2020-03-10 17:31:26 +0800

    第一次提交search

---
+++ nobug-shop-service/nobug-shop-service-search/src/main/java/com/nobug/NobugShopServiceSearchApplication.java
@@ -0,0 +1,13 @@
+package com.nobug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobugShopServiceSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NobugShopServiceSearchApplication.class, args);
+    }
+
+}
---
+++ nobug-shop-service/nobug-shop-service-search/src/test/java/com/nobug/NobugShopServiceSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobugShopServiceSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
