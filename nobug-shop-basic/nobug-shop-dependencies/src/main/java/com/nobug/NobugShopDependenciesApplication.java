39155fc82b4ba11b3874c9430b6da0231d117965
DDD-Rock <laiyifa@bk.ru>
2020-03-10 09:17:48 +0800

    更新了eureka server服务的yml文件

---
+++ nobug-shop-basic/nobug-shop-dependencies/src/main/java/com/nobug/NobugShopDependenciesApplication.java
@@ -0,0 +1,5 @@
+package com.nobug;
+
+
+public class NobugShopDependenciesApplication {
+}
--- nobug-shop-eureka-server/src/main/java/com/nobug/NobugShopEurekaServerApplication.java
+++ nobug-shop-eureka-server/src/main/java/com/nobug/NobugShopEurekaServerApplication.java
@@ -2,10 +2,11 @@ package com.nobug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NobugShopEurekaServerApplication {
 
     public static void main(String[] args) {
