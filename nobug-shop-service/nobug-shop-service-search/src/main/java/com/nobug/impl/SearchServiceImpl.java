9e1f822d50e75376e2838e73531faee2a894e1df
aixiaodevgghh <405956256@qq.com>
2020-03-10 22:24:18 +0800

    第二次提交

---
+++ nobug-shop-service/nobug-shop-service-search/src/main/java/com/nobug/impl/SearchServiceImpl.java
@@ -0,0 +1,19 @@
+package com.nobug.impl;
+
+
+import com.nobug.ISearchService;
+import com.nobug.ResultBean;
+
+public class SearchServiceImpl implements ISearchService {
+
+
+    @Override
+    public ResultBean searchByKeyword(String keyword) {
+        return null;
+    }
+
+    @Override
+    public ResultBean addroduct(Long pid) {
+        return null;
+    }
+}
