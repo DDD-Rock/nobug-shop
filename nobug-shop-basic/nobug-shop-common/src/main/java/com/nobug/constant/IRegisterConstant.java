50ea9c04fef7cb91431b1cf76fc55f5a2ef76901
DDD-Rock <laiyifa@bk.ru>
2020-03-19 20:19:39 +0800

    rock: 加入了两个购物车模块所需要使用的key

--- nobug-shop-basic/nobug-shop-common/src/main/java/com/nobug/constant/IRegisterConstant.java
+++ nobug-shop-basic/nobug-shop-common/src/main/java/com/nobug/constant/IRegisterConstant.java
@@ -2,9 +2,16 @@ package com.nobug.constant;
 
 public interface IRegisterConstant {
 
-    String REGISTER_UUID = "register_uuid";
+    String REGISTER_UUID = "register_uuid"; //邮箱激活时使用的临时key
 
-    String REGISTER_SMS = "register_sms";
+    String REGISTER_SMS = "register_sms";   //验证短信时使用的临时key
 
-    String REDIS_USER_KEY="redis_user_key";
+    String REDIS_USER_KEY="redis_user_key"; //标志用户登录状态、存储用户信息的临时key
+
+    String REDIS_TEMP_CART_UUID="redis_temp_cart_uuid";   //未登录状态下的购物车使用的key
+
+    String REDIS_USER_CART_UID="redis_user_cart_uid";  //登录状态下的用户购物车key
+
+
+
 }
